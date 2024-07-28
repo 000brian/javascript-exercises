84e293c255722a04730b0123eb85f0eb5f11ee45
000brian <brianmaciejwilk@gmail.com>
2024-07-27 22:26:09 -0500

    Finished exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) 
+{
+    for (let i = 1; i < arguments.length; i++)
+    {
+        array = array.filter(element => element !== arguments[i]);
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,48 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) 
+{
+    let start = 0;
+    let end = 0;
+    
+    // Error checking:
+    //  Both parameters must be numbers
+    if (typeof num1 !== "number" || typeof num2 !== "number")
+    {
+        return "ERROR";
+    }
+    //  Both numbers must be integers
+    else if (!Number.isInteger(num1)|| !Number.isInteger(num2))
+    {
+        return "ERROR";
+    }
+    //  Numbers must be positive
+    else if (num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
 
+    // Edge case: numbers are same
+    if (num1 === num2)
+    {
+        return num1;
+    }
+
+    if (num1 > num2)
+    {
+        start = num2;
+        end = num1;
+    }
+    else 
+    {
+        start = num1;
+        end = num2;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
